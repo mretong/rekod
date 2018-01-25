95def8617087b1eba43ee458169944473cb89b0f
Suhairi Abdul Hamid <suhairi81@gmail.com>
2018-01-25 09:46:03 +0800

    warta controller

--- app/Http/Controllers/WartaController.php
+++ app/Http/Controllers/WartaController.php
@@ -3,8 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Warta;
+use App\Blok;
 
 class WartaController extends Controller
 {
-    //
+    public function index()
+    {
+    	$warta = Warta::all();
+
+    	return view('warta.index',compact('warta'));
+    }
 }
---
+++ resources/views/warta/index.blade.php
@@ -0,0 +1,43 @@
+@extends('layouts.members')
+
+
+@section('content')
+
+<h1>Listing Peserta</h1>
+
+<hr />
+
+[ <a href="{{ route('warta.create') }}">Warta Menu</a> ] 
+
+<hr />
+
+<table class="table table-bordered table-striped">
+	<tr>
+		<td>Bil</td>
+		<td>Blok</td>
+		<td>No. Warta</td>
+		<td>Tarikh Warta</td>
+		<td>Tarikh Luput Warta</td>
+		<td>Pilihan</td>
+	</tr>
+
+	@foreach($warta as $news)
+	<tr>
+		<td>{{ $loop->iteration }}</td>
+		<td>{{ $news->blok->id_blok }}</td>
+		<td>{{ $news->id }}</td>
+		<td>{{ $news->t_warta }}</td>
+		<td>{{ $news->t_luputwarta }}</td>
+		
+		<td>
+				{!! Form::open(['route' => ['warta.destroy', $news->id], 'method' => 'delete']) !!}
+					{!! Form::submit('Delete', ['class' => 'btn btn-primary']) !!}				
+				{!! Form::close() !!}{!! Form::open(['route' => ['warta.show', $news->id], 'method' => 'get']) !!}
+					{!! Form::submit('Kemaskini', ['class' => 'btn btn-primary']) !!}				
+				{!! Form::close() !!}
+			
+	</tr>
+	@endforeach
+</table>
+
+@endsection
